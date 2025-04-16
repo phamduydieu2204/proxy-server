ff6f8553cfac4b13adac6aa534804afe16409656
phamduydieu2204 <vidieu.amz@gmail.com>
2025-04-16 17:24:54 +0700

    Cập nhật code.gs để hỗ trợ tìm kiếm trên tất cả các trường

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.post('/api/proxy', async (req, res) => {
   try {
     console.log('Yêu cầu nhận được:', req.body);
     const response = await axios.post(
-      'https://script.google.com/macros/s/AKfycbzU_yptv-NwoVAMS-XlOD5gVBGAC53moVGVwYfYeHEhpmWdS0MqBIEWLFpAafeJX9Og/exec',
+      'https://script.google.com/macros/s/AKfycbwqJ__8VB9vvMreds5CeBdB9h9NEUFIKN-86Vg1meD3DvXdb305OXLNwUmYpMd3maaV/exec',
       req.body,
       {
         headers: {
