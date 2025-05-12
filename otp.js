70a11845a20bc6154e2e90e2f1e7fca6dfa459fe
phamduydieu2204 <vidieu.amz@gmail.com>
2025-05-12 11:13:38 +0700

    tạo thư mục scripts

--- otp.js
+++ otp.js
@@ -131,7 +131,7 @@ async function fetchFinalOtp(email, software, otpSource) {
     expireNote.style.marginTop = "10px";
 
     const deviceNote = document.createElement("div");
-    deviceNote.textContent = result.message ? `Bạn đã đăng nhập ${result.message.split("/")[0].trim()} thiết bị` : "";
+    deviceNote.textContent = result.message ? `${result.message.split("/")[0].trim()} thiết bị` : "";
     deviceNote.style.marginTop = "6px";
 
     container.appendChild(otpCode);
