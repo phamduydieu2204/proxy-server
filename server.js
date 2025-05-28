0abd0488ebabe7b899f1d91c9fadf7205ce0e22b
phamduydieu2204 <vidieu.amz@gmail.com>
2025-05-29 00:03:12 +0700

    cập nhật để deploy Heroku

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.post('/api/proxy', async (req, res) => {
   try {
     console.log('Yêu cầu nhận được:', req.body);
     const response = await axios.post(
-      'https://script.google.com/macros/s/AKfycbz3aL3oWe5bxEA5FHoH5SV29jhZDD32odNS1Ds-SinCT1lEbbO-tnd6BnX0m3pPExLf/exec',
+      'https://script.google.com/macros/s/AKfycbybMjayCWKD41t8uCWGUHUjmXauXxwscf9pipMTfewJU88B1_8AiYOo_N6fayNyxg_O/exec',
       req.body,
       {
         headers: {
